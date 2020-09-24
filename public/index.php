3920a6ff5dd50d2b29018a3862f9ca86c4b698ea
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2020-09-24 10:18:59 +0200

    Added basic connector health check

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+
+use Jtl\Connector\HealthCheck\ConnectorHealthCheck;
+use Noodlehaus\Config;
+
+$rootDir = dirname(__DIR__);
+
+require_once sprintf('%s/vendor/autoload.php', $rootDir);
+
+$configFile = sprintf('%s/config/config.yaml', $rootDir);
+
+if(!is_file($configFile)) {
+    throw new \Exception('Config file missing');
+}
+
+$config = new Config($configFile);
+
+$environment = $config->get('connector.environment');
+$token = $config->get('connector.token');
+$url = $config->get('connector.url');
+
+if(in_array(null, [$environment, $token, $url], true)) {
+    throw new \Exception('environment, token or url in config missing');
+}
+
+(new ConnectorHealthCheck($environment, $token, $url))->checkAndSendResult();
---
+++ src/ConnectorHealthCheck.php
@@ -0,0 +1,78 @@
+<?php
+
+
+namespace Jtl\Connector\HealthCheck;
+
+use Jtl\Connector\Client\Client;
+use Jtl\HealthCheck\AbstractHealthCheck;
+use Jtl\HealthCheck\Result;
+use Jtl\HealthCheck\ResultDetail;
+use Jtl\HealthCheck\ResultMessage;
+
+class ConnectorHealthCheck extends AbstractHealthCheck
+{
+    /**
+     * @var string
+     */
+    protected $environment;
+
+    /**
+     * @var string
+     */
+    protected $connectorToken;
+
+    /**
+     * @var string
+     */
+    protected $connectorUrl;
+
+    /**
+     * ApplicationHealthCheck constructor.
+     * @param string $environment
+     * @param string $connectorToken
+     * @param string $connectorUrl
+     */
+    public function __construct(string $environment, string $connectorToken, string $connectorUrl)
+    {
+        $this->environment = $environment;
+        $this->connectorToken = $connectorToken;
+        $this->connectorUrl = $connectorUrl;
+        parent::__construct();
+    }
+
+    /**
+     * @return Result
+     * @throws \Exception
+     */
+    public function check(): Result
+    {
+        $client = new Client($this->connectorToken, $this->connectorUrl);
+
+        $details = [
+            'env' => new ResultDetail('env', $this->environment),
+            'auth' => new ResultDetail('auth', true),
+            'finish' => new ResultDetail('finish', true)
+        ];
+
+        $messages = [];
+
+        $passed = true;
+        try {
+            $client->authenticate();
+        } catch (\Throwable $ex) {
+            $passed = false;
+            $details['auth']->setValue(false);
+            $messages[] = new ResultMessage('error', 'auth', $ex->getMessage());
+        }
+
+        try {
+            $client->finish();
+        } catch (\Throwable $ex) {
+            $passed = false;
+            $details['finish']->setValue(false);
+            $messages[] = new ResultMessage('error', 'finish', $ex->getMessage());
+        }
+
+        return new Result($passed, array_values($details), $messages);
+    }
+}
